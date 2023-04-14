68e8e80d91f4db07e6c00a835858add62d134a09
Asmahan_Rashed <65909593+Asmahan-Rashed@users.noreply.github.com>
2023-04-14 23:35:21 +0200

    controller for user

---
+++ controllers/user_controller.js
@@ -0,0 +1,79 @@
+const {User}= require('../modles/user');
+
+//................. get courses............................
+
+// const getAllUsers=  async (req, res)=>{
+//     try{
+//         const allUsers= await User.find({});
+//         res.status(200).send(allUsers);
+//     }catch(error){
+//         res.status(400).send(error);
+//     }
+   
+// };
+
+const getUserByID=  async (req, res)=>{
+    try{
+        const user= await User.findOne({_id: req.params.id});
+        res.status(200).send(user);
+    }catch(error){
+        res.status(400).send(error);
+    }
+};
+
+
+//................... Post.........................................
+
+const addUser= async (req, res)=>{
+    try{
+        const user= await User.create(req.body);
+        res.status(200).send(user);
+    }catch(error){
+        res.status(400).send(error);
+    }
+};
+//.................. Put .......................
+const editUserByID=  async (req, res)=>{
+    try{
+        const user= await User.findOne({_id: req.params.id});
+        if(!user){
+            return res.status(404).send("user not found");
+        }
+        if(req.body.userName){
+            user.userName= req.body.userName;       
+        }
+         if(req.body.password){
+            user.password= req.body.password;       
+        } 
+        if(req.body.email){
+            user.email= req.body.email;       
+        } 
+        await user.save();
+        res.status(200).send(user);
+    
+    }catch(error){
+        res.status(400),send(error);
+    } 
+
+};
+
+//............... Delete.....................
+
+const deleteUser=  (req, res)=>{
+   try{
+    const user= User.deleteOne({_id: req.params.id});
+    res.status(200).send(user);
+   }
+   catch(error){
+    res.status(400).send(error);
+   }
+};
+
+
+module.exports= {
+    
+    getUserByID,
+    addUser,
+    editUserByID,
+    deleteUser,
+};
