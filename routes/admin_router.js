0947f84bba9dd08c875628fdfc6a88e815ce90fe
Nourelshehry <76706477+Nourelshehry@users.noreply.github.com>
2023-04-17 00:16:18 +0200

    Add files via upload

---
+++ routes/admin_router.js
@@ -0,0 +1,18 @@
+const { Router }= require("express");
+const adminRouter= Router();
+
+const adminController= require('../controllers/admin_controller');
+
+adminRouter.get('/', adminController.getAllAdmins);
+
+adminRouter.get('/:id', adminController.getAdminByID);
+//................... Post.........................................
+adminRouter.post('/', adminController.addAdmin);
+//.................. Put .......................
+adminRouter.put('/:id', adminController.editAdminByID);
+//............... Delete.....................
+adminRouter.delete('/:id', adminController.deleteAdmin);
+
+adminRouter.put('/:adminId/:orderId', adminController.confirmShipping);
+
+module.exports= {adminRouter};
