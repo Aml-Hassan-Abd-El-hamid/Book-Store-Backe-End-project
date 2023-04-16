3c0834b7472bb67fa6af855c46d788e9016f73a2
Nourelshehry <76706477+Nourelshehry@users.noreply.github.com>
2023-04-17 01:23:50 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,10 +5,14 @@ app.use(express.json());
 const {bookRouter}= require('./routes/book_router');
 const {userRouter}= require('./routes/user_router');
 const {orderRouter}= require('./routes/order_router');
+const {adminRouter}= require('./routes/admin_router');
+
 
 app.use('/api/books', bookRouter);
 app.use('/api/users',userRouter );
 app.use('/api/orders',orderRouter);
+app.use('/api/admins',adminRouter);
+
 
 module.exports={app};
 
